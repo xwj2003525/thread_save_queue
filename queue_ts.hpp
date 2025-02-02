8a4a47901437a082bb61169bcda3970c4428a7a0
xwj2003525 <xwj2003525@gmail.com>
2025-02-02 13:32:44 +0800

    Update queue_ts.hpp

--- queue_ts.hpp
+++ queue_ts.hpp
@@ -2,33 +2,14 @@
 
 #include <chrono>
 #include <condition_variable>
-#include <memory>
 #include <mutex>
 #include <queue>
 #include <type_traits>
 #include <utility>
 
-// 限定只有sharedptr 或者 uniqueptr合法
-// usage Queue<int,shared_ptr> q;
-
 namespace ThreadSafe {
 
-namespace CmpWrapper {
-  template <typename T, typename Cmp>
-  struct SmartPtrCmp{
-    SmartPtrCmp(const Cmp &cmp = Cmp{}):c(cmp){}
-    Cmp c;
-
-    template <typename SmartPtr>
-    bool operator()(const SmartPtr &a, const SmartPtr &b) const {
-        return c(*a, *b); 
-    }
-  };
-};
-
-template <typename T, template<typename...> class SmartPtr, class Cmp=void>
-class Queue {
-
+template <typename T, template <typename> class SmartPtr> class Queue {
   static_assert(std::is_same_v<SmartPtr<T>, std::unique_ptr<T>> ||
                     std::is_same_v<SmartPtr<T>, std::shared_ptr<T>>,
                 "SmartPtr must be either std::unique_ptr or std::shared_ptr");
@@ -47,97 +28,86 @@ public:
   SmartPtr<T>
   pop_until(const std::chrono::time_point<Clock, Duration> &timeout_time);
 
-protected:
-  using Wrapper = CmpWrapper::SmartPtrCmp<T, Cmp>;
-  using Container = std::conditional_t<
-      std::is_void_v<Cmp>, std::queue<SmartPtr<T>>,
-      std::priority_queue<SmartPtr<T>, std::vector<SmartPtr<T>>, Wrapper>>;
+  size_t size() const;
 
-  SmartPtr<T> _pop();
-  Container _container;
-  std::mutex _lock;
+private:
+  std::queue<SmartPtr<T>> _queue;
+  mutable std::mutex _lock;
   std::condition_variable _cv;
 };
 
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
-void Queue<T, SmartPtr, Comparator>::push(const T &value) {
+template <typename T, template <typename> class SmartPtr>
+size_t Queue<T, SmartPtr>::size() const {
   std::lock_guard<std::mutex> lock(_lock);
-  _container.push(SmartPtr<T>(new T(value)));
-  _cv.notify_one();
+  return _queue.size();
 }
 
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
-void Queue<T, SmartPtr, Comparator>::push(T &&value) {
+template <typename T, template <typename> class SmartPtr>
+void Queue<T, SmartPtr>::push(const T &value) {
   std::lock_guard<std::mutex> lock(_lock);
-  _container.push(SmartPtr<T>(new T(std::forward<T>(value))));
-  _cv.notify_one();
-}
-
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
-SmartPtr<T> Queue<T, SmartPtr, Comparator>::_pop() {
-  SmartPtr<T> ret;
-
-  if constexpr (std::is_same_v<decltype(_container), std::queue<SmartPtr<T>>>) {
-    ret = std::move(_container.front());
+  if constexpr (std::is_same_v<SmartPtr<T>, std::unique_ptr<T>>) {
+    _queue.push(std::make_unique<T>(value));
   } else {
-    ret = std::move(const_cast<SmartPtr<T>&>(_container.top()));
+    _queue.push(std::make_shared<T>(value));
   }
-
-  _container.pop();
-
-  return ret;
+  _cv.notify_one();
 }
 
-// 实现 pop_must 方法
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
-SmartPtr<T> Queue<T, SmartPtr, Comparator>::pop_must() {
-  std::unique_lock<std::mutex> lock(_lock);
-  if (_container.empty()) {
-    _cv.wait(lock, [this]() { return !_container.empty(); });
-  }
-  return _pop();
-}
-
-// 实现 pop_try 方法
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
-SmartPtr<T> Queue<T, SmartPtr, Comparator>::pop_try() {
+template <typename T, template <typename> class SmartPtr>
+void Queue<T, SmartPtr>::push(T &&value) {
   std::lock_guard<std::mutex> lock(_lock);
-  if (_container.empty()) {
+  if constexpr (std::is_same_v<SmartPtr<T>, std::unique_ptr<T>>) {
+    _queue.push(std::make_unique<T>(std::forward<T>(value)));
+  } else {
+    _queue.push(std::make_shared<T>(std::forward<T>(value)));
+  }
+  _cv.notify_one();
+}
+
+template <typename T, template <typename> class SmartPtr>
+SmartPtr<T> Queue<T, SmartPtr>::pop_must() {
+  std::unique_lock<std::mutex> lock(_lock);
+  _cv.wait(lock, [this]() { return !_queue.empty(); });
+  SmartPtr<T> value = std::move(_queue.front());
+  _queue.pop();
+  return value;
+}
+
+template <typename T, template <typename> class SmartPtr>
+SmartPtr<T> Queue<T, SmartPtr>::pop_try() {
+  std::lock_guard<std::mutex> lock(_lock);
+  if (_queue.empty()) {
     return nullptr;
   }
-
-  return _pop();
+  SmartPtr<T> value = std::move(_queue.front());
+  _queue.pop();
+  return value;
 }
 
-// 实现 pop_for 方法
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
+template <typename T, template <typename> class SmartPtr>
 template <class Rep, class Period>
-SmartPtr<T> Queue<T, SmartPtr, Comparator>::pop_for(
-    const std::chrono::duration<Rep, Period> &timeout) {
+SmartPtr<T>
+Queue<T, SmartPtr>::pop_for(const std::chrono::duration<Rep, Period> &timeout) {
   std::unique_lock<std::mutex> lock(_lock);
-  if (_cv.wait_for(lock, timeout, [this]() { return !_container.empty(); })) {
-    return _pop();
+  if (_cv.wait_for(lock, timeout, [this]() { return !_queue.empty(); })) {
+    SmartPtr<T> value = std::move(_queue.front());
+    _queue.pop();
+    return value;
   }
   return nullptr;
 }
 
-// 实现 pop_until 方法
-template <typename T, template <typename...> class SmartPtr, typename Comparator>
+template <typename T, template <typename> class SmartPtr>
 template <class Clock, class Duration>
-SmartPtr<T> Queue<T, SmartPtr, Comparator>::pop_until(
+SmartPtr<T> Queue<T, SmartPtr>::pop_until(
     const std::chrono::time_point<Clock, Duration> &timeout_time) {
   std::unique_lock<std::mutex> lock(_lock);
   if (_cv.wait_until(lock, timeout_time,
-                     [this]() { return !_container.empty(); })) {
-
-    return _pop();
+                     [this]() { return !_queue.empty(); })) {
+    SmartPtr<T> value = std::move(_queue.front());
+    _queue.pop();
+    return value;
   }
   return nullptr;
 }
-
-template <typename T, class Cmp = void>
-using SharedQueue = Queue<T, std::shared_ptr, Cmp>;
-
-template <typename T, class Cmp = void>
-using UniqueQueue = Queue<T, std::unique_ptr, Cmp>;
 }; // namespace ThreadSafe
